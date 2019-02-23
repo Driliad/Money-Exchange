9fa6174de7e73367321baa75c238eadbe7c37c70
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2019-02-23 21:28:05 +0300

    Ref

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
    if (currency <= 0) {
       return {};
    }
 
    if (currency > 10000) {
-      return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+      return {
+         error:
+            "You are rich, my friend! We don't have so much coins for exchange"
+      };
    }
 
+   const currencies = {
+      H: 50,
+      Q: 25,
+      D: 10,
+      N: 5,
+      P: 1
+   };
    const money = {};
-   const coinsName = ["H", "Q", "D", "N", "P"];
-   const coinsWorth = [50, 25, 10, 5, 1];
 
-   for (let index = 0; index < coinsWorth.length; index++) {
-      if (currency < coinsWorth[index]) {
-         continue;
+   for (const key in currencies) {
+      if (currency >= currencies[key]) {
+         const count = Math.floor(currency / currencies[key]);
+         money[key] = count;
+
+         currency -= count * currencies[key];
       }
-
-      const count = Math.floor(currency / coinsWorth[index]);
-      currency -= count * coinsWorth[index];
-
-      money[coinsName[index]] = count;
-
-      // Object.defineProperty(money, coinsName[index], { value: count });
    }
 
    return money;
-}
+};
